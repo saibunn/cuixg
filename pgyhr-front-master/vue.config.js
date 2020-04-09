27e904edea52b1d0eb27b60e18d6fca2129a8539
saibunn <cuiwen_sai@163.com>
2020-04-09 21:16:41 +0800

    update pgyhr front

--- pgyhr-front-master/vue.config.js
+++ pgyhr-front-master/vue.config.js
@@ -6,7 +6,7 @@ module.exports = {
         port: 9999,
         proxy: {
             '/pgyhr': {
-                target: 'http://127.0.0.1:8888',  // 请求本地 需要pgyhr后端项目
+                target: 'http://127.0.0.1:8288',  // 请求本地 需要pgyhr后端项目
                 ws: true
             },
             '/foo': {
